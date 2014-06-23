49709a2797a601176f31e42611fd21ee709eac97
Atdhe Shala <atdheshala@gmail.com>
2014-06-23 18:09:39 +0200

    init

---
+++ TestProject/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TestProject
+//
+//  Created by ICK Training 6 on 6/23/14.
+//  Copyright (c) 2014 ICK Training 6. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
